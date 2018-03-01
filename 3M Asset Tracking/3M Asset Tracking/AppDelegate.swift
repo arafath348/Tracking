4d99185dae54608887dae2fd702c06754265921a
wipro <kprajasekaranwipro.cw@mmm.com>
2018-03-01 16:51:05 +0530

    test

--- 3M Asset Tracking/3M Asset Tracking/AppDelegate.swift
+++ 3M Asset Tracking/3M Asset Tracking/AppDelegate.swift
@@ -19,6 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
    
+    //APP Test
 
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
